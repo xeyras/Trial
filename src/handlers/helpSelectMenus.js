1554117e4ae51bbfc71e7a781b8a6230b47d7bf6
xeyras <b0824227@gmail.com>
2026-05-07 23:58:59 +0600

    Update helpSelectMenus.js

--- src/handlers/helpSelectMenus.js
+++ src/handlers/helpSelectMenus.js
@@ -13,7 +13,7 @@ const BACK_BUTTON_ID = "help-back-to-main";
 const ALL_COMMANDS_ID = "help-all-commands";
 const PAGINATION_PREFIX = "help-page";
 const CATEGORY_SELECT_ID = "help-category-select";
-const FOOTER_TEXT = "Made with ❤️";
+const FOOTER_TEXT = "Made by juicew0rld_";
 const SUBCOMMAND_TYPE = 1;
 const SUBCOMMAND_GROUP_TYPE = 2;
 
