4638a51259272237393322be4d2effa3e37f7a5e
xeyras <b0824227@gmail.com>
2026-05-07 18:03:46 +0600

    Update bug.js

--- src/commands/Core/bug.js
+++ src/commands/Core/bug.js
@@ -1,28 +1,27 @@
 import { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, MessageFlags } from 'discord.js';
 import { createEmbed } from '../../utils/embeds.js';
-
 import { InteractionHelper } from '../../utils/interactionHelper.js';
+
 export default {
     data: new SlashCommandBuilder()
         .setName("bug")
         .setDescription("Report a bug or issue with the bot"),
-
     async execute(interaction) {
-        const githubButton = new ButtonBuilder()
-            .setLabel('?? Report Bug on Discord!')
+        const discordButton = new ButtonBuilder()
+            .setLabel('🐛 Report Bug on Discord!')
             .setStyle(ButtonStyle.Link)
-            .setURL('https://discord.gg/fqRxs9eS');
+            .setURL('https://discord.gg/fqRxs9eS'); // ✅ already correct
 
-        const row = new ActionRowBuilder().addComponents(discord);
+        const row = new ActionRowBuilder().addComponents(discordButton); // ✅ fixed variable name
 
         const bugReportEmbed = createEmbed({
-            title: '?? Bug Report',
+            title: '🐛 Bug Report',
             description: 'Found a bug? Please report it on @juicewo_ DM!\n\n' +
             '**When reporting a bug, please include:**\n' +
-            '• ?? Detailed description of the issue\n' +
-            '• ?? Steps to reproduce the pr0blemos\n' +
-            '• ?? Screenshots if applicable. cuz why not!\n' +
-            '• ?? Your bot version and environment ez\n\n' +
+            '• 📝 Detailed description of the issue\n' +
+            '• 🔄 Steps to reproduce the pr0blemos\n' +
+            '• 📸 Screenshots if applicable. cuz why not!\n' +
+            '• 💻 Your bot version and environment ez\n\n' +
             'This helps us fix issues faster and more effectively! EZ',
             color: 'error'
         })
@@ -34,7 +33,3 @@ export default {
         });
     },
 };
-
-
-
-
